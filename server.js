ad4be788fc0f87cf4227e88adbacdd3df16d5e21
CooperAtive <cooper.trowbridge@gmail.com>
2014-04-26 17:07:38 -0700

    update example code with mine

--- server.js
+++ server.js
@@ -3,30 +3,30 @@ var express = require('express'),
 app.use(express.bodyParser());
 
 var ARTICLES = [
-  {
+{
     id: 1,
-    title: 'How to write a JavaScript Framework',
-    author: 'Tomhuda Katzdale',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
+        title: 'How to write a JavaScript Framework',
+        author: 'Tomhuda Katzdale',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+},
+{
     id: 2,
     title: 'Chronicles of an Embereño',
     author: 'Alerik Bryneer',
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
+},
+{
     id: 3,
     title: 'The Eyes of Thomas',
     author: 'Yehuda Katz',
     body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }
+}
 ];
 
 var PHOTOS = [
-  { id: 1, src: "/images/potd.png" },
-  { id: 2, src: "/images/yohuda.jpg" },
-  { id: 3, src: "/images/easter.jpg" }
+{ id: 1, src: "/images/potd.png" },
+{ id: 2, src: "/images/yohuda.jpg" },
+{ id: 3, src: "/images/easter.jpg" }
 ];
 
 // No-brainer auth: server will authenticate with
@@ -35,50 +35,49 @@ var PHOTOS = [
 
 var currentToken;
 app.post('/auth.json', function(req, res) {
+    var body = req.body,
+    username = body.username,
+    password = body.password;
 
-  var body = req.body,
-      username = body.username,
-      password = body.password;
-
-  if (username == 'ember' && password == 'casts') {
+if (username == 'ember' && password == 'casts') {
     // Generate and save the token (forgotten upon server restart).
     currentToken = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     res.send({
-      success: true,
-      token: currentToken
+        success: true,
+        token: currentToken
     });
-  } else {
+} else {
     res.send({
-      success: false,
-      message: 'Invalid username/password'
+        success: false,
+        message: 'Invalid username/password'
     });
-  }
+}
 });
 
 function validTokenProvided(req, res) {
 
-  // Check POST, GET, and headers for supplied token.
-  var userToken = req.body.token || req.param('token') || req.headers.token;
+    // Check POST, GET, and headers for supplied token.
+    var userToken = req.body.token || req.param('token') || req.headers.token;
 
-  if (!currentToken || userToken != currentToken) {
-    res.send(403, { error: 'Invalid token. You provided: ' + userToken });
-    return false;
-  }
+    if (!currentToken || userToken != currentToken) {
+        res.send(401, { error: 'Invalid token. You provided: ' + userToken });
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
 app.get('/articles.json', function(req, res) {
-  if (validTokenProvided(req, res)) {
-    res.send(ARTICLES);
-  }
+    if (validTokenProvided(req, res)) {
+        res.send(ARTICLES);
+    }
 });
 
 // Returns URL to pic of the day.
 app.get('/photos.json', function(req, res) {
-  if (validTokenProvided(req, res)) {
-    res.send(PHOTOS);
-  }
+    if (validTokenProvided(req, res)) {
+        res.send(PHOTOS);
+    }
 });
 
 
